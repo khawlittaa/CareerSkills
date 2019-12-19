714938af4828222d59bee16ac34e27b1a302984d
khawlittaa <khawlahafsiaa@gmail.com>
2019-12-19 20:03:58 +0100

    Initial Commit

---
+++ CareerSkills/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CareerSkills
+//
+//  Created by khawlitta on 12/19/19.
+//  Copyright © 2019 khawla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
